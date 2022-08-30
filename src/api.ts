62c2dfc6da545e09170b589c4a85ccfeb2a0a94f
david.rocker <david.rocker@mykaarma.com>
2022-08-30 18:25:15 +0200

    allow access

--- src/api.ts
+++ src/api.ts
@@ -13,8 +13,8 @@ export const io = new Server(httpServer, {
 });
 import morgan from "morgan";
 import cors from "cors";
-app.use(cors({ origin: ["http://15.237.36.201/", "http://127.0.0.1:3000/"] }));
-//app.use(cors({origin: false}));
+//app.use(cors({ origin: ["http://15.237.36.201", "http://127.0.0.1:3000/"] }));
+app.use(cors({origin: true}));
 //
 var userService = require("./services/userService");
 
